6b5122d4dbfd569aca1e6bdc407bdc3d49601628
Nathan Oliveira <108954624+NateOliveira-dev@users.noreply.github.com>
2025-05-23 20:12:16 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Estou perdido, send help pls! emoji qualquer</h1>
+  return <h1>Estou perdido, send help pls! emoji qualquer</h1>;
+}
+
+function test() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("identação errada");
 }
 
 export default Home;
